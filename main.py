495a9bba4167cd9b4cc7c0357d38219168eea80e
Fabio Alves <fabioarbaiter@gmail.com>
2023-10-28 11:13:29 -0300

    Update thread

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from src.connection import Connection
 from src.voice_recognition import VoiceRecognition
+import threading
 import asyncio
+
+socket = None
+
+
+def start_connection():
+    global socket
+    socket = Connection("ws://10.0.30.88:35500")
+    socket.start()
 
 
 async def main():
-    socket = Connection("ws://10.0.30.88:35500")
-    socket.start()
+    global socket
+    play_thread = threading.Thread(target=start_connection())
+    play_thread.daemon = True
+    play_thread.start()
 
     vr = VoiceRecognition(socket)
     vr.start_recognition()
 
 asyncio.run(main())
-
-
